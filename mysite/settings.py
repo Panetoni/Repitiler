79de41b235ca9ab8131d1a6b19c8ca2e2a727b00
Panetoni <88630444+Panetoni@users.noreply.github.com>
2025-07-03 09:22:47 -0300

    teste db

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,11 +90,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "reptiler_reptiler",
+        "USER": "reptiler_db",
+        "PASSWORD": "repDB1@3",
+        "HOST": "ohio.ufop.br",
+        "PORT": "3306",
     }
 }
 
