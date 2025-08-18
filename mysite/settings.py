e024c75673b3fc4729f048453a5b129d2160a247
Panetoni <88630444+Panetoni@users.noreply.github.com>
2025-08-18 13:23:07 -0300

    mais uma

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = [
     'based-geneva.gl.at.ply.gg',
     '0.0.0.0',
     '200.239.134.154',
-    'reptilerecon.csilab.ufop.br'
+    'reptilerecon.csilab.ufop.br',
+    '.reptilerecon.csilab.ufop.br',
 ]
 
 
